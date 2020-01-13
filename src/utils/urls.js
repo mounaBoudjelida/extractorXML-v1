0dbe7e047fb1fc6096c529fb7b831e9b35e3f33f
Your Name <youremail@domain.com>
2020-01-13 13:12:27 -0700

    first commit

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,5 @@
-//export const deploimentLink="http://localhost:8080/";
-export const deploimentLink="https://polar-badlands-05508.herokuapp.com/";
+export const deploimentLink="http://localhost:8080/";
+//export const deploimentLink="https://desolate-tundra-06706.herokuapp.com/";
 export const urlFeedBDD=deploimentLink+"epreuves/epreuves";
 export const urlLoginAdmin=deploimentLink+"login/loginAdmin";
 export const urlGetEpreuves=deploimentLink+"epreuves/epreuves";
