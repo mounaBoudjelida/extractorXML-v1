984bd64ba4470a4087ae26f6463d3900d7a3ae50
Your Name <youremail@domain.com>
2020-01-13 13:41:03 -0700

    Updates

---
+++ precache-manifest.fda001d6f8812eff9b96a24694166099.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d024e71e33d5e502173ff327ae4683",
+    "url": "/extractorXML-v1/index.html"
+  },
+  {
+    "revision": "bca86c7efb4cb533dc7d",
+    "url": "/extractorXML-v1/static/css/2.66e62f61.chunk.css"
+  },
+  {
+    "revision": "72a3505b9e2048e278a4",
+    "url": "/extractorXML-v1/static/css/main.d531fdb1.chunk.css"
+  },
+  {
+    "revision": "bca86c7efb4cb533dc7d",
+    "url": "/extractorXML-v1/static/js/2.b8981e66.chunk.js"
+  },
+  {
+    "revision": "8bf4122c1c935c11d3e585bc7556b767",
+    "url": "/extractorXML-v1/static/js/2.b8981e66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72a3505b9e2048e278a4",
+    "url": "/extractorXML-v1/static/js/main.ac2fedc4.chunk.js"
+  },
+  {
+    "revision": "f5ed2990483431ad3a30",
+    "url": "/extractorXML-v1/static/js/runtime-main.3b6a802c.js"
+  },
+  {
+    "revision": "91efa191648811866106935f4c1c38dd",
+    "url": "/extractorXML-v1/static/media/PHOTO-PAGE-ACCUEIL.91efa191.jpg"
+  }
+]);
